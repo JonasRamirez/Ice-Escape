9c55f40879563ce802b0a14ec1bdd2cecc8212e9
JonasRamirez <jxra0003@ce.pucmm.edu.do>
2026-03-30 16:34:05 -0400

    Se me olvidó añadir esto

---
+++ Assets/UI/UILevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILevel : MonoBehaviour {
+
+    public Text levelText;
+
+    void Start()
+    {
+        levelText.text = "Nivel " + LevelManager.currentLevel;
+        Invoke("HideText", 2f);
+    }
+
+    void HideText()
+    {
+        levelText.gameObject.SetActive(false);
+    }
+}
