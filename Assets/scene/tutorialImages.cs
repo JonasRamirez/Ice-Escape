56dff48323807d0b23919968fb1d79994eadef43
Bndi <yenluisguzmandiaz@gmail.com>
2026-03-30 10:55:57 -0400

    Interfaz de Tutorial basica

---
+++ Assets/scene/tutorialImages.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialImages : MonoBehaviour {
+
+	int imageNum = 0;
+	public GameObject[] images;
+
+	public void nextImage()
+{
+    changeImage(true);
+}
+
+public void start()
+	{
+		for (int i = 0; i < images.Length; i++)
+		{
+			if (i == imageNum)
+			{
+				images[i].SetActive(true);
+			}
+			else
+			{
+				images[i].SetActive(false);
+			}
+		}
+	}
+
+public void previousImage()
+{
+    changeImage(false);
+}
+
+public void changeImage(bool next)
+	{
+		if (imageNum < images.Length && imageNum >= 0)
+		{
+			if (next)
+			{
+				imageNum++;
+			}
+			else
+			{
+				imageNum--;
+			}
+
+			for (int i = 0; i < images.Length; i++)
+			{
+				if (i == imageNum)
+				{
+					images[i].SetActive(true);
+				}
+				else
+				{
+					images[i].SetActive(false);
+				}
+			}
+		}
+		else if (imageNum >= images.Length)
+		{
+			imageNum = images.Length - 1;
+		}
+		else if (imageNum < 0)
+		{
+			imageNum = 0;
+		}
+		images[imageNum].SetActive(true);
+		
+	}
+
+}
+
+
